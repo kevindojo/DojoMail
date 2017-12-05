09c309df211a7db0a428d7d6db575383fc70564d
KevinPham <kevinpham15@gmail.com>
2017-12-05 14:57:00 -0800

    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title: String = 'Dojo Mail';
 
-  email = {
-    
-  }
+  emails = [
+    {email: 'bill@gates.com', important: true, subject: 'New Windows', content: 'Windows XI will launch in the year 2100' },
+    {email: 'ada@lovelace.com', important: true, subject: 'Programming', content: 'Enchantress of Numbers'},
+    {email: 'john@carmac.com', important: false, subject: 'Updated Algo', content: 'New Algorithim for shadow volumes'},
+    {email: 'gabe@newel.com', important: false, subject: 'HL3!', content: "Just kidding..."}
+]
 
+  //make a dictionary
 
 
 }
